2daf8a825f8843348a6698059a6bd21839eb99aa
twrwr <49817528+twrwr@users.noreply.github.com>
2022-06-25 03:00:50 +0800

    Fix the Default Cube Mesh
    - 6 Face version, should be correct now...

--- CS300_Vulkan/src/main.cpp
+++ CS300_Vulkan/src/main.cpp
@@ -112,8 +112,14 @@ int main(int argc, char argv[])
         0,1,2
     };
 
-   
-    std::vector<oGFX::Vertex>boxverts{
+    constexpr glm::vec3 redColor = glm::vec3{ 1.0f,0.0f,0.0f };
+    constexpr glm::vec3 dirX = glm::vec3{ 1.0f,0.0f,0.0f };
+    constexpr glm::vec3 dirY = glm::vec3{ 0.0f,1.0f,0.0f };
+    constexpr glm::vec3 dirZ = glm::vec3{ 0.0f,0.0f,1.0f };
+
+    std::vector<oGFX::Vertex> boxverts
+    {
+        /*
     	oGFX::Vertex{ {-0.5,-0.5,-0.5}, { -1.0f,-1.0f,-1.0f },{ 1.0f,0.0f,0.0f }, { 0.0f,0.0f } },
     	oGFX::Vertex{ { 0.5,-0.5,-0.5}, { 0.0f,-1.0f,0.0f },{ 1.0f,0.0f,0.0f }, { 0.0f,0.0f } },
     	oGFX::Vertex{ {-0.5, 0.5,-0.5}, { -1.0f,0.0f,-1.0f },{ 1.0f,0.0f,0.0f }, { 0.0f,0.0f } },
@@ -123,10 +129,77 @@ int main(int argc, char argv[])
     	oGFX::Vertex{ { 0.5,-0.5, 0.5}, { 1.0f,-1.0f,1.0f },{ 1.0f,0.0f,0.0f }, { 0.0f,0.0f } },
     	oGFX::Vertex{ {-0.5, 0.5, 0.5}, { -1.0f,1.0f,1.0f },{ 1.0f,0.0f,0.0f }, { 0.0f,1.0f } },
     	oGFX::Vertex{ { 0.5, 0.5, 0.5}, { 1.0f,1.0f,1.0f },{ 1.0f,0.0f,0.0f }, { 1.0f,1.0f } }
+        */
+
+        // 
+        oGFX::Vertex{ {-0.5, 0.5, 0.5}, dirZ, redColor, { 0, 1 }, dirX },
+        oGFX::Vertex{ { 0.5, 0.5, 0.5}, dirZ, redColor, { 1, 1 }, dirX },
+        oGFX::Vertex{ {-0.5,-0.5, 0.5}, dirZ, redColor, { 0, 0 }, dirX },
+        oGFX::Vertex{ { 0.5,-0.5, 0.5}, dirZ, redColor, { 1, 0 }, dirX },
+        //
+        oGFX::Vertex{ { 0.5, 0.5,-0.5}, -dirZ, redColor, { 0, 1 }, -dirX },
+        oGFX::Vertex{ {-0.5, 0.5,-0.5}, -dirZ, redColor, { 1, 1 }, -dirX },
+        oGFX::Vertex{ { 0.5,-0.5,-0.5}, -dirZ, redColor, { 0, 0 }, -dirX },
+        oGFX::Vertex{ {-0.5,-0.5,-0.5}, -dirZ, redColor, { 1, 0 }, -dirX },
+        //
+        oGFX::Vertex{ { 0.5, 0.5, 0.5}, dirX, redColor, { 0, 1 }, -dirZ },
+        oGFX::Vertex{ { 0.5, 0.5,-0.5}, dirX, redColor, { 1, 1 }, -dirZ },
+        oGFX::Vertex{ { 0.5,-0.5, 0.5}, dirX, redColor, { 0, 0 }, -dirZ },
+        oGFX::Vertex{ { 0.5,-0.5,-0.5}, dirX, redColor, { 1, 0 }, -dirZ },
+        //
+        oGFX::Vertex{ {-0.5, 0.5,-0.5}, -dirX, redColor, { 0, 1 }, dirZ },
+        oGFX::Vertex{ {-0.5, 0.5, 0.5}, -dirX, redColor, { 1, 1 }, dirZ },
+        oGFX::Vertex{ {-0.5,-0.5,-0.5}, -dirX, redColor, { 0, 0 }, dirZ },
+        oGFX::Vertex{ {-0.5,-0.5, 0.5}, -dirX, redColor, { 1, 0 }, dirZ },
+        //
+        oGFX::Vertex{ {-0.5, 0.5,-0.5}, dirY, redColor, { 0, 1 }, dirX },
+        oGFX::Vertex{ { 0.5, 0.5,-0.5}, dirY, redColor, { 1, 1 }, dirX },
+        oGFX::Vertex{ {-0.5, 0.5, 0.5}, dirY, redColor, { 0, 0 }, dirX },
+        oGFX::Vertex{ { 0.5, 0.5, 0.5}, dirY, redColor, { 1, 0 }, dirX },
+        //
+        oGFX::Vertex{ {-0.5,-0.5, 0.5}, -dirY, redColor, { 0, 1 }, dirX },
+        oGFX::Vertex{ { 0.5,-0.5, 0.5}, -dirY, redColor, { 1, 1 }, dirX },
+        oGFX::Vertex{ {-0.5,-0.5,-0.5}, -dirY, redColor, { 0, 0 }, dirX },
+        oGFX::Vertex{ { 0.5,-0.5,-0.5}, -dirY, redColor, { 1, 0 }, dirX }
+
+        /*
+        // Data here is dumped from my engine somewhere...
+        [0]	{position=[-0.5 0.5 0.5] normal=[0 0 1] tangent=[1 0 0] ...}     {bitangent=[0 1 0] uv=[0 1]
+        [1]	{position=[0.5 0.5 0.5] normal=[0 0 1] tangent=[1 0 0] ...}      {bitangent=[0 1 0] uv=[1 1]
+        [2]	{position=[-0.5 -0.5 0.5] normal=[0 0 1] tangent=[1 0 0] ...}    {bitangent=[0 1 0] uv=[0 0]
+        [3]	{position=[0.5 -0.5 0.5] normal=[0 0 1] tangent=[1 0 0] ...}     {bitangent=[0 1 0] uv=[1 0]
+
+        [4]	{position=[0.5 0.5 -0.5] normal=[-8.74228e-08 0 -1] tangent=[-1 0 8.9407e-08] ...}       {bitangent=[0 1 0] uv=[0 1]
+        [5]	{position=[-0.5 0.5 -0.5] normal=[-8.74228e-08 0 -1] tangent=[-1 0 8.9407e-08] ...}      {bitangent=[0 1 0] uv=[1 1]
+        [6]	{position=[0.5 -0.5 -0.5] normal=[-8.74228e-08 0 -1] tangent=[-1 0 8.9407e-08] ...}      {bitangent=[0 1 0] uv=[0 0]
+        [7]	{position=[-0.5 -0.5 -0.5] normal=[-8.74228e-08 0 -1] tangent=[-1 0 8.9407e-08] ...}     {bitangent=[0 1 0] uv=[1 0]
+
+        [8]	{position=[0.5 0.5 0.5] normal=[1 0 -4.37114e-08] tangent=[-2.98023e-08 0 -1] ...}        {bitangent=[0 1 0] uv=[0 1]
+        [9]	{position=[0.5 0.5 -0.5] normal=[1 0 -4.37114e-08] tangent=[-2.98023e-08 0 -1] ...}       {bitangent=[0 1 0] uv=[1 1]
+        [10	{position=[0.5 -0.5 0.5] normal=[1 0 -4.37114e-08] tangent=[-2.98023e-08 0 -1] ...}       {bitangent=[0 1 0] uv=[0 0]
+        [11 {position=[0.5 -0.5 -0.5] normal=[1 0 -4.37114e-08] tangent=[-2.98023e-08 0 -1] ...}      {bitangent=[0 1 0] uv=[1 0]
+
+        [12]	{position=[-0.5 0.5 -0.5] normal=[-1 0 -4.37114e-08] tangent=[-2.98023e-08 0 1] ...}     {bitangent=[0 1 0] uv=[0 1]
+        [13]	{position=[-0.5 0.5 0.5] normal=[-1 0 -4.37114e-08] tangent=[-2.98023e-08 0 1] ...}      {bitangent=[0 1 0] uv=[1 1]
+        [14]	{position=[-0.5 -0.5 -0.5] normal=[-1 0 -4.37114e-08] tangent=[-2.98023e-08 0 1] ...}    {bitangent=[0 1 0] uv=[0 0]
+        [15]	{position=[-0.5 -0.5 0.5] normal=[-1 0 -4.37114e-08] tangent=[-2.98023e-08 0 1] ...}     {bitangent=[0 1 0] uv=[1 0]
+
+        [16]	{position=[-0.5 0.5 -0.5] normal=[0 1 -4.37114e-08] tangent=[1 0 0] ...}   {bitangent=[0 -2.98023e-08 -1] uv=[0 1]
+        [17]	{position=[0.5 0.5 -0.5] normal=[0 1 -4.37114e-08] tangent=[1 0 0] ...}    {bitangent=[0 -2.98023e-08 -1] uv=[1 1]
+        [18]	{position=[-0.5 0.5 0.5] normal=[0 1 -4.37114e-08] tangent=[1 0 0] ...}    {bitangent=[0 -2.98023e-08 -1] uv=[0 0]
+        [19]	{position=[0.5 0.5 0.5] normal=[0 1 -4.37114e-08] tangent=[1 0 0] ...}     {bitangent=[0 -2.98023e-08 -1] uv=[1 0]
+
+        [20]	{position=[-0.5 -0.5 0.5] normal=[0 -1 -4.37114e-08] tangent=[1 0 0] ...}     {bitangent=[0 -2.98023e-08 1] uv=[0 1]
+        [21]	{position=[0.5 -0.5 0.5] normal=[0 -1 -4.37114e-08] tangent=[1 0 0] ...}      {bitangent=[0 -2.98023e-08 1] uv=[1 1]
+        [22]	{position=[-0.5 -0.5 -0.5] normal=[0 -1 -4.37114e-08] tangent=[1 0 0] ...}    {bitangent=[0 -2.98023e-08 1] uv=[0 0]
+        [23]	{position=[0.5 -0.5 -0.5] normal=[0 -1 -4.37114e-08] tangent=[1 0 0] ...}     {bitangent=[0 -2.98023e-08 1] uv=[1 0]
+        */
     };
     for (auto& v : boxverts) { v.norm = glm::normalize(v.norm); }
 
-    static std::vector<uint32_t> boxindices{
+    static std::vector<uint32_t> boxindices
+    {
+        /*
         1,0,2
         ,3,1,2
         ,5,1,3
@@ -139,6 +212,19 @@ int main(int argc, char argv[])
         ,3,6,7
         ,5,4,0
         ,5,0,1
+        */
+        0,2,1,
+        1,2,3,
+        4,6,5,
+        5,6,7,
+        8,10,9,
+        9,10,11,
+        12,14,13,
+        13,14,15,
+        16,18,17,
+        17,18,19,
+        20,22,21,
+        21,22,23
     };
 
     std::vector<Point3D> pts{
@@ -449,9 +535,13 @@ int main(int argc, char argv[])
             
             //ImGui::ShowDemoWindow();
             
-            ImGui::Begin("Test");
+            ImGui::Begin("Entity List");
             for (auto& entity: renderer.entities)
             {
+                ImGui::PushID(entity.modelID);
+                
+                ImGui::BulletText("[%d] ", entity.modelID);
+                ImGui::SameLine();
                 switch (entity.modelID)
                 {
                 case 0:
@@ -466,11 +556,13 @@ int main(int argc, char argv[])
                 default:
                 break;
                 }
-                auto id = std::to_string(entity.modelID);
-                ImGui::DragFloat3(std::string("Position"+ id).c_str(), glm::value_ptr(entity.pos), 0.01f);
-                ImGui::DragFloat3(("Scale"+id).c_str(),  glm::value_ptr(entity.scale), 0.01f);
-                ImGui::DragFloat(("theta"+id).c_str(),  &entity.rot);
-                ImGui::DragFloat3(("Rotation Vec"+id).c_str(),  glm::value_ptr(entity.rotVec));
+                
+                ImGui::DragFloat3("Position", glm::value_ptr(entity.pos), 0.01f);
+                ImGui::DragFloat3("Scale", glm::value_ptr(entity.scale), 0.01f);
+                ImGui::DragFloat("theta", &entity.rot);
+                ImGui::DragFloat3("Rotation Vec", glm::value_ptr(entity.rotVec));
+
+                ImGui::PopID();
             }
             ImGui::End();
             renderer.DrawGUI();
