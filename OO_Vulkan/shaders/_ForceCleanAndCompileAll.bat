f0963ab17dad2f9a573fe8a0bf8cc10c76549f6d
twrwr <49817528+twrwr@users.noreply.github.com>
2022-09-11 00:51:48 +0800

    Create _ForceCleanAndCompileAll.bat
    Batch file to clear all and compile all

---
+++ OO_Vulkan/shaders/_ForceCleanAndCompileAll.bat
@@ -0,0 +1,26 @@
+@echo off
+
+set OUTPUT=bin\
+
+cd %OUTPUT%
+for %%f in (*.spv) do (
+	DEL %%f	1>nul
+)
+cd "../"
+
+set OUTPUT=bin\
+rem Create a "bin" folder if it does not exist, for shader binary output
+if not exist %OUTPUT% (
+  mkdir %OUTPUT%
+)
+
+echo [92m####################################################[0m
+echo [92m# Compiling all shaders to SPIR-V binary format... #[91m
+
+for %%i in (*.vert *.frag *.comp *.geom) do (
+	"%VULKAN_SDK%\Bin\glslc.exe" --target-env=vulkan1.2 -std=460 -O "%%~i" -o "bin/%%~i.spv"
+)
+
+echo [92m# Shader Compilation Ended...                      #[0m
+echo [92m####################################################[0m
+pause
